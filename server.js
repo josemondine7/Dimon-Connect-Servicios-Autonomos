02ff4d58caaea9b8d5f672ad6477358b6c319644
josemondine7 <josemondine723@gmail.com>
2026-08-20 06:32:35 -0300

    Change server port and update static file serving

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// Servir archivos desde la carpeta "público"
+app.use(express.static(path.join(__dirname, 'público')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// Ruta principal → abre índice.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'público', 'índice.html'));
 });
 
 app.listen(PORT, () => {
